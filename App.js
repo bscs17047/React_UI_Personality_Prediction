d01baca6c75af4d169f7b9a0b4daaee812bdfafd
bscs17047 <36041868+bscs17047@users.noreply.github.com>
2020-12-11 12:50:11 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FileUpload from './Screens/FileUpload';
+import About from './Screens/About';
+
+export default function App() {
+
+  //return <FileUpload />;
+  return <Team />;
+  //return <About />;
+
+}
